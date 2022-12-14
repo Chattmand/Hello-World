e3a594492dbe6becf714134bf4e9135b9002d871
DamarChattman <damarchattman@gmail.com>
2022-12-14 11:30:57 -0800

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// write your code here
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080, () => {
+    console.log("server is listening on server port 8080")
+}); //the server object listens on port 8080
+
